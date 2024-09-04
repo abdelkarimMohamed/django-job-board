55c2c9f2b49dd2dfe0eee6e2f458e116abcd43c5
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-09-04 20:33:47 +0300

    add contact-us form and send mail

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
+from .models import Info
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def send_message(request):
-    pass
+
+    myinfo=Info.objects.last()
+    if request.method == 'POST':
+        subject=request.POST['subject']
+        email=request.POST['email']
+        message=request.POST['message']
+
+        print(subject)
+        print(email)
+        print(message)
+        
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [email],
+        )
+
+    return render(request,'contact/contact.html',{'myinfo':myinfo})
--- project/settings.py
+++ project/settings.py
@@ -135,3 +135,10 @@ Media_ROOT=os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_USER='abdelkarimmohamedali1993@gmail.com'
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
+EMAIL_PORT='587'
